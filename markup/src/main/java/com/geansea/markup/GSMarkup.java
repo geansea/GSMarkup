7c7114d6cf4e378b52d8c1bc27da9fa6497c6763
geansea <geansea@126.com>
2017-11-11 23:08:21 +0800

    init project

---
+++ markup/src/main/java/com/geansea/markup/GSMarkup.java
@@ -0,0 +1,13 @@
+package com.geansea.markup;
+
+import android.text.Spanned;
+
+public class GSMarkup {
+    public static Spanned fromMarkup(String source) {
+        return null;
+    }
+
+    public static String escapeMarkup(CharSequence text) {
+        return null;
+    }
+}
